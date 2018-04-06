54406acfed18b8464fb3381ba1104d8c3cc968a6
Puyan Wei <puyanwei@gmail.com>
2018-04-06 11:24:41 +0100

    created an app-server-status component and made it globally available in main.js

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,8 @@
 <template>
-<p>Server Status: {{ status }}</p>
+<app-server-status></app-server-status>
 </template>
 
 <script>
-export default {
-  data: () => {
-    return {
-      status: 'Critical',
-    };
-  },
-};
 </script>
 
 <style>
