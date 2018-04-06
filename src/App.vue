66393f83d035dea4d077bb656f2f790770f70e77
Puyan Wei <puyanwei@gmail.com>
2018-04-06 16:38:57 +0100

    completed exercise refactoring html into components

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,31 @@
 <template>
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12">
-                <header>
-                    <h1>Server Status</h1>
-                </header>
-            </div>
-        </div>
+        <app-header></app-header>
         <hr>
         <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
+            <servers></servers>
+            <server-status></server-status>
         </div>
         <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
+import Header from './Header.vue';
+import Footer from './Footer.vue';
+import Servers from './Servers.vue';
+import ServerStatus from './ServerStatus.vue';
+
+export default {
+  // prettier-ignore
+  components: {
+    'app-header': Header,
+    'app-footer': Footer,
+    'servers': Servers,
+    'server-status': ServerStatus,
+  }
+};
 </script>
 
 <style>
