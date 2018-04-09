b54f3ad1859cd8baaf289d0b8f463cf7d56cff70
Puyan Wei <puyanwei@gmail.com>
2018-04-09 10:37:05 +0100

    setup environment for 'communication between components' section of course

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,26 @@
 <template>
     <div class="container">
-        <app-header></app-header>
-        <hr>
         <div class="row">
-            <servers></servers>
-            <server-status></server-status>
+            <div class="cols-xs-12">
+                <app-user></app-user>
+            </div>
         </div>
-        <hr>
-        <app-footer></app-footer>
     </div>
 </template>
 
 <script>
-import appHeader from './components/shared/Header.vue';
-import appFooter from './components/shared/Footer.vue';
-import Servers from './components/server/Servers.vue';
-import ServerStatus from './components/server/ServerStatus.vue';
+import appUser from './components/User.vue';
 
 export default {
-  // prettier-ignore
   components: {
-    appHeader,
-    appFooter,
-    Servers,
-    ServerStatus,
-  }
+    appUser,
+  },
 };
 </script>
 
 <style>
+div.component {
+  border: 1px solid black;
+  padding: 30px;
+}
 </style>
---
+++ src/components/User.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="component">
+      <h1>The User Component</h1>
+      <p>I'm an awesome user!</p>
+      <hr>
+      <div class="row">
+          <div class="cols-xs-12 col-sm-6">
+              <user-detail></user-detail>
+          </div>
+          <div class="cols-xs-12 col-sm-6">
+              <user-edit></user-edit>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+import userDetail from './UserDetail.vue';
+import userEdit from './UserEdit.vue';
+
+export default {
+  components: {
+    userDetail,
+    userEdit,
+  },
+};
+</script>
+
+<style scoped>
+div {
+  background-color: lightblue;
+}
+</style>
