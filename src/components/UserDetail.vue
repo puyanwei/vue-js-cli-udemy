045ab3c35f3565f7ce6a8adfd216d8ab67fc11fe
Puyan Wei <puyanwei@gmail.com>
2018-04-09 11:12:56 +0100

    created a function in UserDetail that reverses the name, that refers to the prop that is passed in

--- src/components/UserDetail.vue
+++ src/components/UserDetail.vue
@@ -2,13 +2,21 @@
     <div class="component">
       <h3>You may view the user details here</h3>
       <p>Many details...</p>
-      <p>User Name: {{ name }}</p>
+      <p>User Name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
 export default {
   props: ['name'],
+  methods: {
+    switchName() {
+      return this.name
+        .split('')
+        .reverse()
+        .join('');
+    },
+  },
 };
 </script>
 
